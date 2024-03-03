ca2145a4bd5727000fefebd2697400e4c1f2587e
Anara <berdali.aygerim9@gmail.com>
2024-03-03 11:30:18 +0000

    projectData updated

--- src/assets/projectData.js
+++ src/assets/projectData.js
@@ -5,92 +5,79 @@ import webApisQuiz from "./images/08-web-apis-challenge-demo.gif";
 import gazetteer from "./images/gazetteer.png";
 import companyDirectory from "./images/Company_director.png";
 import workDayScheduler from "./images/work-day-scheduler-demo.gif";
+import passwordGenerator from "./images/password-generator-demo.png"
 import portfolioBootstrap from "./images/portfolio-bootstrap.png"
 
 export default [
   {
     id: "0",
-    description: "Application to record running workouts",
-    projectType: "Group Project",
+    title: "MyRun",
+    description: "JS/jQuery/CSS/APIs",
     deployedUrl: "https://anarab.github.io/MyRun/",
     githubUrl: "https://github.com/AnaraB/MyRun",
-    tools: ["JS", "JQuery", "Bootstrap", "API"],
-    title: "MyRun",
     image: myRunApp,
   },
   {
     id: "1",
-    description:
-      "Node.js command-line application that generates a webpage and displays employee's basic info",
-    githubUrl: "https://github.com/AnaraB/OOP_team_profile_generator",
-    projectType: "bootcamp assignment",
-    tools: ["JS", "Jest", "OOP", "Bootstrap", "Inquirer"],
     title: "Team_profile_generator",
+    description: "Inquirer/OOP/Jest/Bootstrap",
+    githubUrl: "https://github.com/AnaraB/OOP_team_profile_generator",
     image: teamProfileGenerator,
   },
   {
     id: "2",
-    description:
-      "Application that retrieves weather data from third-party weather API",
-    projectType: "bootcamp assignment",
+    title: "Weather-dashboard",
+    description:"jQuery/API/Bootstrap",
     deployedUrl: "https://anarab.github.io/weather-dashboard/",
     githubRepo: "https://github.com/AnaraB/weather-dashboard",
-    tools: ["HTML", "JS", "Bootstrap", "JQuery", "API"],
-    title: "Weather-dashboard",
     image: weatherDashboard,
   },
   {
     id: "3",
-    description: "Timed coding quiz with multiple-choice questions",
+    title: "Code quiz",
+    description: "Vanilla JS/CSS",
     deployedUrl: "https://anarab.github.io/web-apis-quiz/",
     githubUrl: "https://github.com/AnaraB/web-apis-quiz",
-    projectType: "bootcamp assignment",
-    tools: ["HTML", "JS", "JQuery", "CSS"],
-    title: "web-apis-quiz",
     image: webApisQuiz,
   },
   {
     id: "4",
-    description:
-      "One-page, interactive map application provides various features for selected coutry.",
-    projectType: "independent project",
+    title: "Gazetteer",
+    description: "APIs/JQuery/CSS/AJAX/PHP/XAMPP",
     deployedUrl: "https://gazetteer.anara-berdalieva.co.uk/",
     githubRepo: "https://github.com/AnaraB/gazetteer",
-    tools: ["HTML/JS/CSS", "APIs", "JQuery", "Leaflet", "Bootstrap/CSS", "AJAX", "XAMPP", "PHP"],
-    title: "Gazetteer",
     image: gazetteer,
   },
   {
     id: "5",
-    description:
-      "The Employee Management System app, that allows users to view, add, edit, and delete employee records, departments, and locations",
+    title: "Company Directory",
+    description: "JS/Bootstrap/PHP/SQL/XAMPP",
     deployedUrl: "https://companydirectory.anara-berdalieva.co.uk/",
     githubUrl: "https://github.com/AnaraB/companydirectory",
-    projectType: "independent project",
-    tools: ["HTML/JS/CSS","Boostrap", "PHP", "SQL", "XAMPP"],
-    title: "Company Directory",
     image: companyDirectory,
   },
   {
     id: "6",
-    description:
-      "A simple calendar application that allows a user to save events for each hour of the day.  ",
-    projectType: "",
+    title: "Work day planner",
+    description: "HTML/jQuery/Bootstrap",
     deployedUrl: "https://anarab.github.io/Work-day-scheduler/",
     githubRepo: "https://github.com/AnaraB/Work-day-scheduler",
-    tools: ["HTML", "JavaScript"],
-    title: "Work-day-scheduler",
     image: workDayScheduler,
   },
   {
     id: "7",
-    description:
-      "Basic portfolio website",
+    title: "My portfolio project",
+    description: "HTML/JavaScript/Bootstrap",
     deployedUrl: "https://anarab.github.io/portfolio-b/",
     githubUrl: "https://github.com/AnaraB/portfolio-b",
-    projectType: "portfolio",
-    tools: ["HTML", "CSS", "JavaScript", "Bootstrap"],
-    title: "My portfolio project",
     image: portfolioBootstrap
+  },
+  {
+    id: "8",
+    title: "Password Generator",
+    description: "HTML/Vanilla JS/CSS",
+    deployedUrl: "https://anarab.github.io/password-generator/",
+    githubUrl: "https://github.com/AnaraB/password-generator",
+    image: passwordGenerator
   }
 ];
