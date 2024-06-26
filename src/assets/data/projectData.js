ee559ad854782b94ba8656749b3fa47590c33dc7
Anara <berdali.aygerim9@gmail.com>
2024-06-26 14:38:52 +0100

    notes and github finder projects added

--- src/assets/data/projectData.js
+++ src/assets/data/projectData.js
@@ -7,13 +7,15 @@ import companyDirectory from "../images/portfolio-img/Company_director.png";
 import workDayScheduler from "../images/portfolio-img/work-day-scheduler-demo.gif";
 import passwordGenerator from "../images/portfolio-img/password-generator-demo.png"
 import anarkaGB from "../images/portfolio-img/anarkaGB.png";
+import githubFinder from "../images/portfolio-img/github_finder.jpeg";
+import notes from "../images/portfolio-img/notes-demo.png";
 
 
 export default [
   {
     id: "0",
     title: "MyRun",
-    description: "JS / jQuery / CSS / APIs",
+    description: "JS / jQuery / CSS / API",
     deployedUrl: "https://anarab.github.io/MyRun/",
     githubUrl: "https://github.com/AnaraB/MyRun",
     image: myRunApp,
@@ -37,20 +39,19 @@ export default [
   {
     id: "3",
     title: "Gazetteer",
-    description: "JS / APIs / JQuery / CSS / AJAX / PHP",
+    description: "JS / API / JQuery / AJAX / PHP",
     deployedUrl: "https://gazetteer.anara-berdalieva.co.uk/",
     githubUrl: "https://github.com/AnaraB/gazetteer",
     image: gazetteer,
-   
   },
   {
     id: "4",
-    title: "Anarkas Goodness Bites",
-    description: "HTML / CSS",
-    deployedUrl: "https://anarab.github.io/anarkasGB/",
-    githubUrl: "https://github.com/AnaraB/anarkasGB",
-    image: anarkaGB
-  
+    title: "Notes",
+    description: "React / Material UI / Vite",
+    deployedUrl: "https://gregarious-kringle-3302e3.netlify.app/",
+    githubUrl: "https://github.com/AnaraB/Notes",
+    image: notes,
+    
   },
   {
     id: "5",
@@ -86,7 +87,26 @@ export default [
     githubUrl: "https://github.com/AnaraB/Work-day-scheduler",
     image: workDayScheduler,
     
-  }
+  },
+  {
+    id: "9",
+    title: "GitHub Profile Finder",
+    description: "React / Tailwind CSS / REST API / Context API",
+    deployedUrl: "https://github-profile-finder-lovat.vercel.app/",
+    githubUrl: "https://github.com/AnaraB/GitHub-profile-finder",
+    image: githubFinder,
+    
+  },
+  {
+    id: "10",
+    title: "Anarkas Goodness Bites",
+    description: "HTML / CSS",
+    deployedUrl: "https://anarab.github.io/anarkasGB/",
+    githubUrl: "https://github.com/AnaraB/anarkasGB",
+    image: anarkaGB
+  
+  },
+ 
 ];
 
 
--- src/components/ProjectItem.jsx
+++ src/components/ProjectItem.jsx
@@ -1,30 +1,34 @@
-import React from "react"
+import React from "react";
 
-function ProjectItem({projectInfo}) {
- 
-  const {id, title, description, deployedUrl, githubUrl, image} = projectInfo;
+function ProjectItem({ projectInfo }) {
+  const { id, title, description, deployedUrl, githubUrl, image } = projectInfo;
   return (
-
     <div className="col-lg-4  col-sm-6">
       <div className="project-card py-2 " key={projectInfo.id}>
-        <img  src={projectInfo.image} alt={projectInfo.title} className="img-fluid"/>
+        <img
+          src={projectInfo.image}
+          alt={projectInfo.title}
+          className="img-fluid"
+        />
         <div className="project-card-caption">
           <div className="project-card-caption-content">
-            <a href={projectInfo.githubUrl}  target="_blank">	<i className="fab fa-github"></i></a>
-            <br />
-            <a href={projectInfo.deployedUrl} target="_blank"><i className="fas fa-link"></i></a>
-            <p className="project-description-tools">{projectInfo.description}</p>
-
+            <div className="project-card-icons">
+              <a href={projectInfo.githubUrl} target="_blank">
+                {" "}
+                <i className="fab fa-github"></i>
+              </a>
+              <a href={projectInfo.deployedUrl} target="_blank">
+                <i className="fas fa-link fa-2x"></i>
+              </a>
+            </div>
+            <p className="project-description-tools">
+              {projectInfo.description}
+            </p>
           </div>
-
         </div>
-
       </div>
-
     </div>
-
-  )
-  
+  );
 }
 
-export default ProjectItem
+export default ProjectItem;
